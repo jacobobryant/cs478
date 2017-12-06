943ffb54568aa67dfe3eca93bf79a85b93bb9615
Jacob O'Bryant <jacobobryant7@gmail.com>
2017-12-05 17:06:14 -0700

    Add sql stuff w/ incremental feature adding

--- DataCollecting/get_features.py
+++ DataCollecting/get_features.py
@@ -8,18 +8,23 @@ from my_textstat import textstatistics as textstat
 from os.path import join
 from functools import partial
 from code import interact
-from joblib import Memory
 from lxml import etree
 from dateutil.parser import parse as parse_date
 from datetime import datetime
 from urllib.parse import urlencode
 import traceback
-import secrets
 import pandas
+from os.path import realpath, dirname, join
+from data import links, pageviews
+import multiprocessing
+import os
+import inspect
+import sqlite3
 
+path_from_root = lambda *args: join(dirname(dirname(realpath(__file__))), *args)
+dbname = path_from_root("DataCollecting", "features.db")
 search_engine_id = "017178850987838406603:vp0vn2nince"
-
-memory = Memory(cachedir='joblib_cache', verbose=0)
+google_custom_search_api_key = 'AIzaSyAQOvYZXc3z90zNf3urf8j2i1wuu2CGUDw'
 
 dfall_names = pandas.read_csv('all_names.csv')
 baby_names = dfall_names.values
@@ -50,7 +55,7 @@ def get_story_names():
     return set([i for i in good_story_names if i not in names_to_ignore])
 story_names = get_story_names()
 
-def feat_gender(link):
+def featGender(link):
     _, speakerlink = link.split('/talks/')
 
     #Get the speaker's first name.
@@ -124,18 +129,18 @@ def feat_gender(link):
     else: #if 0.4 <= mfratio <= 5, AND it's not in the top (LONG) list of popular unisex names, we're just going to forget it.
         return 'Unknown'
 
-def feat_polarity(text):
+def featPolarity(text):
     Text = TextBlob(text)
     return round(Text.sentiment.polarity, 2)
 
-def feat_subjectivity(text):
+def featSubjectivity(text):
     Text = TextBlob(text)
     return round(Text.sentiment.subjectivity, 2)
 
-def feat_word_count(text):
+def featWordCount(text):
     return textstat(text).lexicon_count()
 
-def feat_flesch_reading_ease(text):
+def featFleschReading(text):
     for i in [';',':']: #We're going to help them out. Semicolons and colons become periods so they count as their own sentence.
         text = text.replace(i, '.')
     
@@ -149,15 +154,15 @@ def book_refs(speech):
 
 def count_books(book_list, speech):
     return len([b for b in book_refs(speech) if b in book_list])
-feat_OT = partial(count_books, OT_books)
-feat_NT = partial(count_books, NT_books)
-feat_BoM = partial(count_books, BoM_books)
-feat_DyC = partial(count_books, DyC_books)
-feat_PoGP = partial(count_books, PoGP_books)
-feat_all_scripture_count = partial(count_books, OT_books + NT_books +
+featOT = partial(count_books, OT_books)
+featNT = partial(count_books, NT_books)
+featBoM = partial(count_books, BoM_books)
+featDC = partial(count_books, DyC_books)
+featPoGP = partial(count_books, PoGP_books)
+featAllScriptureCount = partial(count_books, OT_books + NT_books +
                                    BoM_books + DyC_books + PoGP_books)
 
-def feat_name_mentions(text):
+def featStoryNames(text):
     #Remove anything in parentheses
     text = re.sub(r'[\(\[][^)]*?[\)\]]', '', text) #Remove anything in parentheses or brackets. Remember the ?, which makes it not greedy.
     #Uppercase AND not (1) beginning a sentence, (2) in a scripture reference, (3) preceeded by 'Elder' or 'President'.
@@ -165,8 +170,7 @@ def feat_name_mentions(text):
     story_name_mentions = set(close_to_what_we_want).intersection(story_names)
     return len(story_name_mentions)
 
-@memory.cache
-def feat_time_elapsed(link):
+def featTalkingSpeed(link):
     long_text = read_file(link, 'Speeches', 'html')
     try:
         url = list(set(re.findall(r'https:\/\/.*\.mp3', long_text)))[0]
@@ -177,14 +181,14 @@ def feat_time_elapsed(link):
 
     length_audiofile = r.headers['Content-length']
     seconds = int(length_audiofile) / 6000
-    return seconds
+    return featWordCount(long_text) / seconds
 
-def feat_appeal_to_authority(text):
+def featAuthorityMentions(text):
     authority_mentions = re.findall(r'\b(President|Elder|Prophet|of the Quorum of the Twelve)\b', text)
     return len(authority_mentions)
 
 #This isn't precisely the ratio, it's actually (we_count**2) / you_count. This will weight it in favor of lots of 'we's, even if there are a lot of 'you's too.
-def feat_we_to_you_ratio(text):
+def featWeToYouRatio(text):
     we_count = len(re.findall(r'\bwe\b', text.lower()))
     you_count = len(re.findall(r'\byou\b', text.lower()))
 
@@ -195,17 +199,17 @@ def feat_we_to_you_ratio(text):
 
     return score
 
-def feat_word_quantity(text):
+def featWordQuantity(text):
     words = re.findall(r'\b\w+\b', text)
     freq_set = set(words)
     return len(freq_set)
 
-def feat_use_of_I(text):
+def featFirstPersonPronoun(text):
     say_I = re.findall(r'\bI\b', text)
     return len(say_I)
 
 # This is good at getting quotes, but misses things in quotations.
-def feat_words_in_italics(long_text):
+def featPercentInItalics(long_text):
 
     #If there's a Notes section, we'll remove it. Italics will be in references, we don't want those.
     try:
@@ -219,21 +223,22 @@ def feat_words_in_italics(long_text):
     all_words_in_italics = ' '.join(l) #We're going to combine this into a single string.
     all_words_in_italics = re.sub(r'\<.{1,3}\>',' ', all_words_in_italics) #and take out the italics and bold parts.
 
-    return feat_word_count(all_words_in_italics)
+    return featWordCount(all_words_in_italics) / featWordCount(long_text)
 
-def feat_quotes_in_quotation_marks(text):
+def featPercentInQuotes(text):
     all_quotes = re.findall(r'\".+?\"', text)
     all_words_in_quotes = ' '.join(all_quotes).replace('"','')
-    return feat_word_count(all_words_in_quotes)
+    return feat_word_count(all_words_in_quotes) / featWordCount(long_text)
 
-def feat_days_elapsed(tree):
+def featDaysElapsed(tree):
     raw_date = tree.xpath('//meta[@name="date"]')[0].get('content')
     date = parse_date(raw_date)
     return (datetime.now() - date).days
 
-@memory.cache
-def get_num_results(name):
-    params = {'key': secrets.google_custom_search_api_key,
+def featNameSearchResults(tree):
+    return None
+    name = feat_name(tree)
+    params = {'key': google_custom_search_api_key,
               'cx': search_engine_id,
               'q': name}
     query = ("https://www.googleapis.com/customsearch/v1?" +
@@ -242,11 +247,17 @@ def get_num_results(name):
     response = requests.get(query)
     return int(response.json()['queries']['request'][0]['totalResults'])
 
-def feat_name_search_results(tree):
-    return get_num_results(feat_name(tree))
+def featPageviews(link):
+    return None
 
-def feat_name(tree):
-    return tree.xpath('//meta[@name="author"]')[0].get('content').strip("'")
+def featMonthGiven(link):
+    return None
+
+def featYearGiven(link):
+    return None
+
+def featSpeakerPosition(link):
+    return None
 
 def read_file(link, parent, ext):
     path = join(parent, re.sub(r'^/talks/', '', link).rstrip('/').lower() + '.' + ext)
@@ -255,45 +266,119 @@ def read_file(link, parent, ext):
 get_long_text = lambda link: read_file(link, 'Speeches', 'html')
 get_speech = lambda link: read_file(link, 'JustWordsSpeeches', 'txt')
 
-if __name__ == '__main__':
-    from data import links, pageviews
-    import multiprocessing
-    import os
-    import inspect
+def convert(x):
+    try:
+        return float(x)
+    except ValueError:
+        return x
 
-    titleize = lambda fn_name: re.sub(r'^feat_', '', fn_name).title().replace('_', '')
-    titles, fns = zip(*[(titleize(member), globals()[member])
-                        for member in globals() if member.startswith('feat_')])
 
-    def get_features(link):
+def init_db():
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+
+    c.execute('create table if not exists features (' +
+              'link text primary key, ' + 
+              ', '.join(['{} blob'.format(t) for t in titles]) +
+              ')')
+
+    # TODO auto add new columns
+
+    #csvs = [path_from_root('DataCollecting', 'features.csv'),
+    #        path_from_root('STATS.csv'),
+    #        path_from_root('Learning', 'FULL_speech_popularity.csv')]
+
+    #for p in csvs:
+    #    with open(p, 'r') as f:
+    #        data = [[convert(x) for x in line.split(',')]
+    #                for line in f.read().strip().split('\n')]
+    #    headers, data = (data[0], data[1:])
+    #    for i, row in enumerate(data):
+    #        row = {h: x for h, x in zip(headers, row)}
+    #        link = links[i] if p.endswith('features.csv') else row['Speech']
+
+    #        r = c.execute('select count(*) from features where link = ?', (link,))
+    #        if r.fetchone()[0] == 0:
+    #            c.execute('insert into features (link) values (?)', (link,))
+
+    #        insert_data = {k: v for k, v in row.items() if k in titles}
+    #        c.execute('update features set ' +
+    #                  ', '.join([k + '=?' for k in insert_data.keys()]) +
+    #                  ' where link=?',
+    #                  list(insert_data.values()) + [link])
+
+    conn.commit()
+    conn.close()
+
+
+def fill_in_features():
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    colnames = ('link',) + titles
+    all_empty = set()
+
+    nmissing = 0
+    nupdated = 0
+    n_missing_talks = 0
+
+    for row in c.execute('select ' + ', '.join(colnames) + ' from features').fetchall():
+        link = row[0]
+        empty_cols = [col for col, val in zip(colnames, row) if val is None]
+
+        if not empty_cols:
+            continue
+
         try:
             long_text = get_long_text(link)
             speech = get_speech(link)
             tree = etree.HTML(long_text)
-
-            def dispatch(f):
-                argname = inspect.getargspec(f)[0][0]
-                if argname in ('link', 'self'):
-                    return f(link)
-                elif argname == 'long_text':
-                    return f(long_text)
-                elif argname == 'tree':
-                    return f(tree)
-                return f(speech)
-
-            return [dispatch(f) for f in fns]
         except Exception as e:
             if 'No such file or directory' not in str(e):
                 traceback.print_exc()
             else:
-                print(e, file=sys.stderr)
-            return []
-    
-    pool = multiprocessing.Pool()
-    results = [r for r in pool.map(get_features, links)
-    #results = [r for r in map(get_features, links)
-               if r is not None]
-    with open('features.csv', 'w') as outfile:
-        print('Pageviews', *titles, sep=',', file=outfile)
-        for nviews, features in zip(pageviews, results):
-            print(nviews, *features, sep=',', file=outfile)
+                #print(e, file=sys.stderr)
+                pass
+            n_missing_talks += 1
+            continue
+
+        nmissing += len(empty_cols)
+        all_empty = all_empty.union(set(empty_cols))
+        fns = [globals()['feat' + col] for col in empty_cols]
+
+        def dispatch(f):
+            argname = inspect.getargspec(f)[0][0]
+            if argname in ('link', 'self'):
+                return f(link)
+            elif argname == 'long_text':
+                return f(long_text)
+            elif argname == 'tree':
+                return f(tree)
+            return f(speech)
+        
+        values = [dispatch(f) for f in fns]
+        print(empty_cols, values, link)
+        c.execute('update features set ' +
+                  ', '.join([k + '=?' for k in empty_cols]) +
+                  ' where link=?', values + [link])
+        nupdated += len([x for x in values if x is not None])
+
+    print()
+    print("n missing talks:", n_missing_talks)
+    print("missing features before update:", all_empty)
+    print("n missing features before update:", nmissing)
+    print("n updated features:", nupdated)
+    conn.commit()
+    conn.close()
+
+def sql_interact():
+    conn = sqlite3.connect(dbname)
+    c = conn.cursor()
+    interact(local=locals())
+    conn.close()
+
+if __name__ == '__main__':
+    titles, fns = zip(*[(re.sub(r'^feat', '', member), globals()[member])
+                        for member in globals() if member.startswith('feat')])
+    init_db()
+    #sql_interact()
+    fill_in_features()
